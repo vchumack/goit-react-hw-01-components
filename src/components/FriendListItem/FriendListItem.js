ca865e9f29b56aa0ad37237a8de3664615edf34b
Valeriya Druchinina <vchumack@glmedia.org>
2022-09-14 00:17:36 +0300

    add

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export const FriendListItem = ({ isOnline, avatar, name }) => {
   return (
-    <li className="item">
+    <li className={css.item}>
       <span className="status">{isOnline}</span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
